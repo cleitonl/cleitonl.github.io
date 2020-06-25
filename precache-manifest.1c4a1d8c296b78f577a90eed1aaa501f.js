fabcdde6776582fa1dfed797650cbe700ae28d0b
Your Name <you@example.com>
2020-06-25 10:32:30 -0300

    primeiro commit

---
+++ precache-manifest.1c4a1d8c296b78f577a90eed1aaa501f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da43b6b9e882336801145581470e77ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e33d813f95c07fb0268",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "014ff8b35c79bf9fe144",
+    "url": "/static/js/2.496e94c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.496e94c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e33d813f95c07fb0268",
+    "url": "/static/js/main.40e5d801.chunk.js"
+  },
+  {
+    "revision": "fae39dd47ad9a00bb02c",
+    "url": "/static/js/runtime-main.c24f648c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
